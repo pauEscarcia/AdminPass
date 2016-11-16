21015facc78f3bcacd2a661d12dc7c0e2345fb31
ozonetw <ozonetw@gmail.com>
2016-11-15 16:01:54 -0800

    KeyIcon Forms

--- adminPassword/src/adminpassword/Locket.java
+++ adminPassword/src/adminpassword/Locket.java
@@ -9,12 +9,12 @@ package adminpassword;
  *
  * @author Exelsion
  */
-public class Main extends javax.swing.JFrame {
+public class Locket extends javax.swing.JFrame {
 
     /**
-     * Creates new form Login
+     * Creates new form Locket
      */
-    public Main() {
+    public Locket() {
         initComponents();
     }
 
@@ -28,51 +28,39 @@ public class Main extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jLabel2 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(800, 600));
+        setBackground(new java.awt.Color(255, 253, 215));
 
         jPanel1.setBackground(new java.awt.Color(255, 253, 215));
-        jPanel1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        jPanel1.setInheritsPopupMenu(true);
-        jPanel1.setLayout(null);
 
-        jLabel2.setFont(new java.awt.Font("Existence Light", 0, 24)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(218, 116, 116));
-        jLabel2.setText("Bienvenid@");
-        jPanel1.add(jLabel2);
-        jLabel2.setBounds(330, 530, 260, 28);
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/generalBG.png"))); // NOI18N
 
-        jButton1.setBackground(new java.awt.Color(208, 109, 127));
-        jButton1.setFont(new java.awt.Font("Glamourgirl", 0, 24)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(255, 255, 255));
-        jButton1.setText("Ingresar");
-        jButton1.setMaximumSize(new java.awt.Dimension(240, 240));
-        jButton1.setMinimumSize(new java.awt.Dimension(240, 240));
-        jPanel1.add(jButton1);
-        jButton1.setBounds(270, 420, 260, 80);
-
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/LocketO10.png"))); // NOI18N
-        jPanel1.add(jLabel1);
-        jLabel1.setBounds(170, 37, 480, 330);
-
-        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/bgPink.png"))); // NOI18N
-        jPanel1.add(jLabel3);
-        jLabel3.setBounds(0, -340, 800, 1290);
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jLabel1)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jLabel1)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 810, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 893, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -95,30 +83,26 @@ public class Main extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Locket.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Locket.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Locket.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Locket.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Main().setVisible(true);
+                new Locket().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
